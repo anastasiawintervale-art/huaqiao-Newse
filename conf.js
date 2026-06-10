691570fd9b7fd5a53515de4018bba8e38d9979ec
anastasiawintervale-art <anastasiawintervale@gmail.com>
2026-06-10 09:21:21 +0000

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,95 @@
+
+var _appKey = "unoehb";
+
+var _op_addr = "../js/openinstall-" + _appKey + ".js";
+
+// 默认绑定的域名
+var init_domain = "514op.youzi.top";
+
+//默认渠道号
+var _default_channel = "514op";
+
+// 不统计的OP渠道码
+var clearOpList = [
+ 
+
+
+];
+
+// 指定的渠道码【 关键字：渠道码】；若是短链接,关键字为主域名(不含后缀)
+var domainList = {
+'514op': '514op',
+'m50':'514op',
+'kajhhsj':'521cpc88',
+'hbbbwbs':'521cpc99',
+'akflvad':'521cpc88',
+'onczqts':'521cpc99',
+'dzlooo':'521cpc88',
+'lgeuyfp':'521cpc99',
+'potzgz':'515cpc03',
+'xuriuj':'515cpc15',
+'efvksq':'517cpc01',
+'kypsq':'520cpa99',
+'jkrbhv':'520cpa99',
+'dmwyqz':'521cpc88',
+'pqgeud':'521cpc99',
+'owdajsb':'521cpc88',
+'gbwspmv':'521cpc99',
+'khnzmce':'jp111',
+'rwnzzeh':'jp111',
+'ilxgtca':'jp112',
+'k1':'jp111',
+'sjrkmnh':'jp112',
+'s8':'jp112',
+'rtanpab':'jp113',
+'q11':'jp113',
+'n1':'jp113',
+'p1':'jp111',
+'f1':'jp112',
+'s1':'jp111',
+'q1':'jp112',
+'t1':'jp113',
+
+
+
+
+
+
+
+};
+
+//子渠道
+var _op_subChannel = [
+    
+
+];
+
+// 指定的51统计码【渠道码：固定的51】，变更时只要加渠道码，后面的值固定
+var _la51 = {
+    "521cpc88":{
+        "id":"3PyVLmcKzdJMs3jD",
+        "ck":"3PyVLmcKzdJMs3jD",
+    },
+    
+    "521cpc99":{
+        "id":"3PyVJ4xzeTsircOs",
+        "ck":"3PyVJ4xzeTsircOs",
+    },
+    
+    "jp111":{
+        "id":"3Q6Cn82Zh8LMNFMs",
+        "ck":"3Q6Cn82Zh8LMNFMs",
+    },    
+    
+    "jp112":{
+        "id":"3Q82r7eSOKiib97e",
+        "ck":"3Q82r7eSOKiib97e",
+    },      
+    
+    "jp113":{
+        "id":"3QAW0sDAhOaa3kIy",
+        "ck":"3QAW0sDAhOaa3kIy",
+    },     
+    
+    
+};
